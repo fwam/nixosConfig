d22400d8faa37a8e3b58eacc55241bc998bd85af
Xenia <xenia@queereen.dev>
2024-04-28 02:52:12 +0200

    fix: zsh didn't work correctly.

--- home-manager/modules/shell/default.nix
+++ home-manager/modules/shell/default.nix
@@ -1,4 +1,20 @@
 { config, ... }: {
+    programs.zsh = {
+        enable = true;
+        initExtra = ''
+        bindkey "^[[1;5C" forward-word
+        bindkey "^[[1;5D" backward-word
+        ''; 
+        
+        shellAliases = {
+            cd = "z";
+            cat = "bat";
+            python3 = "python";
+            tf = "terraform";
+            k8s = "kubectl";
+        };
+    };
+
     programs.starship = {
         enable = true;
         enableZshIntegration = true;
